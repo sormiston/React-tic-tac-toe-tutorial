257f635d2f4736440cda4b5046108b15bf82cc11
SOrmiston <sean.david.ormiston@gmail.com>
2020-07-26 00:54:24 -0400

    Board refactored

---
+++ src/Square.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+export default Square
